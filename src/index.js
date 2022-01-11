533ab430ca687aeb258bfc373858dccc65d9b510
etozhesady <sadyrovanvar@gmail.com>
2022-01-12 00:44:37 +0600

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let num = n.toString()
+	let array = num.split('')
+	let result = ''
+	for (let i = array.length - 1; i >= 0; i--) {
+		if (array[i] !== '-' && array[i] !== '+') {
+			result += array[i]
+		}
+	}
+	return Number(result)
 }
